74b0369aea0c006cc1156e9f67bd2d712403a80a
Jelena Jovicic <jejovicic@gmail.com>
2021-06-27 00:14:20 +0200

    Working version

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
-# rock-paper-scissors
-TOP Project #2
+# The Odin Project - Rock, Paper, Scissors
+
+A simple implementation of a "rock paper scissors" game, built for for [The Odin Project](https://www.theodinproject.com).
+
+Currently played from console only.
+
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Instructions](#instructions)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [Things to remember](#things-to-remember)
+  - [Things to work on](#things-to-work-on)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### Instructions
+
+- Begin with a function called computerPlay that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
+
+- Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock".
+
+  - Use prompt() to get input from the user. Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
+
+  - Important note: you want to return the results of this function call, not console.log() them. To test this function console.log the results.
+
+- Write a NEW function called game(). Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end. Loops are covered later, so just call your playRound function 5 times in a row. At this point you should be using console.log() to display the results of each round and the winner at the end.
+
+- Feel free to create more “helper” functions if you think it would be useful.
+
+
+
+### Screenshot
+
+No GUI yet, to be added.
+
+### Links
+
+- [Solution](https://github.com/je-jo/rock-paper-scissors)
+- Live site to be added.
+
+## My process
+
+### Built with
+
+### Things to remember
+
+If variable name occurs as a function parameter it doesn't need to be declared again with the let keyword. 
+
+### Things to work on
+
+[] Pick computer choice from array
+[] Add loop instead of calling function 5 times
+[] Add GUI...
+
+### Useful resources
+
+- [Minireset](https://awesomeopensource.com/project/jgthms/minireset.css?categoryPage=29) - A tiny modern CSS reset. 
+
+## Author
+
+- [Github](https://github.com/je-jo)
+- [Frontend Mentor](https://www.frontendmentor.io/profile/je-jo)
+- [Codepen](https://codepen.io/je-jo)
--- script.js
+++ script.js
@@ -1,97 +1,84 @@
-/* 
-
-
-- begin with a function playMachine that will randomly return either rock, paper or scissors
-
-- get input from user, either rock paper or scissors, case insensintive
-
-*/
-
-
-let playerSelection = 0;
-let machineSelection = 0;
+// global variables
 let playerScore = 0;
-let machineScore = 0;
+let computerScore = 0;
+
+// // helper functions
+// function uppercase() {
+  
+// }
 
 
-function playMachine() {                                //computer randomly choose rock paper or scissors
-    machineSelection = Math.floor(Math.random() * 3);          //returns a random integer between 0 and 2
-    if (machineSelection === 0) {                                  //asigns string values to function results
-        machineSelection = "ROCK";
-    } else if (machineSelection === 1) {
-        machineSelection = "PAPER";
+
+// function to randomly choose rock paper or scissors
+function computerPlay() {
+    let computerSelection = Math.floor(Math.random() * 3);           //returns a random integer between 0 and 2
+    if (computerSelection === 0) {                                  //assigns string values to function results
+        return "ROCK";
+    } else if (computerSelection === 1) {
+        return "PAPER";
     } else {
-        machineSelection = "SCISSORS";
+        return "SCISSORS";
     }
-    console.log(`The Machine chose ${machineSelection}`)
 }
 
 
+// function to play a single round
+function playRound(playerSelection, computerSelection) {
 
-function play() {                            //get input from user
-    playerSelection = window.prompt("Please choose rock, paper or scissors?", "");
+    playerSelection = prompt("Rock, paper, scissors?", "");   //get input from user  
     playerSelection = playerSelection.toUpperCase();
-    console.log(`You chose ${playerSelection}`)
-}
+    console.log(`You chose ${playerSelection}`);
+
+    computerSelection = computerPlay();
+    console.log(`The Machine chose ${computerSelection}`);
 
 
-
-
-function playRound() {                          //play round
-    play();
-    playMachine();
-
-    if (playerSelection === machineSelection) {
-        alert("It's a tie! Play again!");
+    if (playerSelection === computerSelection) {
+        return "It's a tie! play again!";
     }
-
     else if (playerSelection === "ROCK") {
-        if (machineSelection === "PAPER") {
-            machineScore += 1;
-            alert(`You lose! The Machine has ${machineScore} points`);
-        } else if (machineSelection === "SCISSORS") {
+        if (computerSelection === "PAPER") {
+            computerScore += 1;
+            return `You lose! Paper beats rock! The Machine has ${computerScore} points.`;
+        } else if (computerSelection === "SCISSORS") {
             playerScore += 1;
-            alert(`You win! You have ${playerScore} points`);
+            return `You win! Rock beats scissors! You have ${playerScore} points.`;
         }
     }
     else if (playerSelection === "PAPER") {
-        if (machineSelection === "SCISSORS") {
-            machineScore += 1;
-            alert(`You lose! The Machine has ${machineScore} points`);
-        } else if (machineSelection === "ROCK") {
+        if (computerSelection === "SCISSORS") {
+            computerScore += 1;
+            return `You lose! Scissors beat paper! The Machine has ${computerScore} points.`;
+        } else if (computerSelection === "ROCK") {
             playerScore += 1;
-            alert(`You win! You have ${playerScore} points`);
+            return `You win! Paper beats rock! You have ${playerScore} points.`;
         }
     }
     else if (playerSelection === "SCISSORS") {
-        if (machineSelection === "ROCK") {
-            machineScore += 1;
-            alert(`You lose! The Machine has ${machineScore} points`);
-        } else if (machineSelection === "PAPER") {
+        if (computerSelection === "ROCK") {
+            computerScore += 1;
+            return `You lose! Rock beats scissors! The Machine has ${computerScore} points.`;
+        } else if (computerSelection === "PAPER") {
             playerScore += 1;
-            alert(`You win! You have ${playerScore} points`);
+            return `You win! Scissors beat paper! You have ${playerScore} points.`;
         }
     }
-    console.log(`You have ${playerScore} points.
-    The Machine has ${machineScore} points.`)
+    console.log(`You: ${playerScore}; The Machine: ${computerScore};`);
 }
 
 
-
-
+// function to play 5 rounds and declare winner, add loop later
 function game() {
     playRound();
     playRound();
     playRound();
     playRound();
     playRound();
-    if (playerScore > machineScore) {
-        console.log("You win!");
-    } else if (playerScore < machineScore) {
-        console.log("You lose!");
+    if (playerScore > computerScore) {
+        return "You fought the Machine and you WON!";
+    } else if (playerScore < computerScore) {
+        return "You fought the Machine and you LOST!";
     } else {
-        console.log("It's a tie!");
+        return "It's a tie!";
     }
 }
-
-
